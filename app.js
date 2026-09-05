cee4287f9537ca0c6c386023617fcf607993b907
Jerry Flores <jerryflores@MacBook-Air-de-Jerry.local>
2026-09-05 15:40:05 -0600

    Feature: Add San Sebastián del Oeste 360° virtual tour

--- app.js
+++ app.js
@@ -9,7 +9,8 @@
   const PANO_MAP = {
     'scene_ahuacatlan': 'ahuacatlan.jpg',
     'scene_barranca_de_oro': 'barranca-de-oro.jpg',
-    'scene_barranca_oro_nuevo': 'oro.jpg'
+    'scene_barranca_oro_nuevo': 'oro.jpg',
+    'scene_san_sebastian': 'san-sebastian.jpg'
   };
 
   const els = {
